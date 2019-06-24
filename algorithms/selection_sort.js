98ed5b10d04d3b097d45b5dbe2848f8edb40d140
Deepak Govindarajan <deepak.g@krds.fr>
2019-06-24 11:50:07 +0530

    KU

--- algorithms/selection_sort.js
+++ algorithms/selection_sort.js
@@ -1,3 +1,24 @@
+/*
+arr[] = 64 25 12 22 11
+
+// Find the minimum element in arr[0...4]
+// and place it at beginning
+11 25 12 22 64
+
+// Find the minimum element in arr[1...4]
+// and place it at beginning of arr[1...4]
+11 12 25 22 64
+
+// Find the minimum element in arr[2...4]
+// and place it at beginning of arr[2...4]
+11 12 22 25 64
+
+// Find the minimum element in arr[3...4]
+// and place it at beginning of arr[3...4]
+11 12 22 25 64 
+
+*/
+
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
     // Set default active minimum to current index.
